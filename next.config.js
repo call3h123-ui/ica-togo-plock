7004c27de18ce975b5f1ace823e7ffa4d12a01e1
call3h123-ui <call3h123@gmail.com>
2026-01-10 23:53:02 +0100

    Redirect root URL to /togo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/togo',
+        permanent: false,
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
